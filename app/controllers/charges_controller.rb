c3a97523a03d6721b3b69f013cda30d07cc51ab6
kykoyo <kykoyo1992@gmail.com>
2015-01-13 16:38:40 -0800

    debugged on charges_controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -10,19 +10,19 @@ class ChargesController < ApplicationController
         #make new charge with info from view
         email=params[:stripeEmail]
         token=params[:stripeToken]
-        day=params[:day]
+        day=params[:day].to_i
 
         @charge=Charge.new(email: email, token: token, day: day)
 
         #search for UserTable if exists or not
-        user=User.find_by_email(@charge.email)
+        @user=User.find_by_email(@charge.email)
 
-        #create user if not used before, otherwise add counter
-        if user.nil?
+        #create user with random string if not used before, otherwise add counter
+        if @user.nil?
             password=(0...8).map { (65 + rand(26)).chr }.join            
-            user=User.new(email: email, password: password, user_type: 'daypass', counter: day)
-        elsif user.user_type=='daypass'
-            user.counter+=day
+            @user=User.new(email: email, password: password, user_type: 'daypass', counter: day)
+        elsif @user.user_type=='daypass'
+            @user.counter+=day
         else
             flash[:errors]<<'You are already a member of us.'
         end
@@ -43,23 +43,26 @@ class ChargesController < ApplicationController
             :card  => params[:stripeToken]
         )
 
+        puts @amount
         charge = Stripe::Charge.create(
             :customer    => customer.id,
             :amount      => @amount,
             :description => 'Rails Stripe customer',
             :currency    => 'usd'
         )
-
         rescue Stripe::CardError => e
             flash[:error]<< e.message
             redirect_to charges_path
         end
-
         if flash[:errors].size==0
             @charge.save
-            user.save
+            @user.save
             render 'show'
         end
 
+    end
+
+    def show
+        @user=User.find(params[:id])
     end
 end
