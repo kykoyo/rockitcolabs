3742a1318a78825d5e28f374e32c8df8a06259c1
kykoyo <kykoyo1992@gmail.com>
2015-01-29 14:54:21 -0800

    small fix part 1

--- db/migrate/20150113211532_change_format_in_charge_table.rb
+++ db/migrate/20150113211532_change_format_in_charge_table.rb
@@ -1,6 +1,6 @@
 class ChangeFormatInChargeTable < ActiveRecord::Migration
   def change
     rename_column :charges, :charge_type, :day
-    change_column :charges, :day, :integer
+    change_column :charges, :count, :integer
   end
 end
